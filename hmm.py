f9597cb0de91d2706f507776cdd001b9d68aeb3c
parthnp <parth.np5@gmail.com>
2022-02-07 22:50:50 +0530

    hmm_learn and requirements files added

---
+++ hmm.py
@@ -0,0 +1,148 @@
+from configparser import RawConfigParser
+from argparse import ArgumentParser
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+import os
+import logging
+
+class HMM_Client:
+    logger = logging.getLogger(__name__)
+    def __init__(self, test_in_sentences:list, output_file:str, saved_model_path:str, ):
+        self.saved_model_path = saved_model_path
+        self.test_in_sentences = test_in_sentences or []
+        self.output_file = output_file
+        self.emission_prob_df = None
+        self.transition_prob_df = None
+        self.results = []
+        self.pos_tags = []
+        
+    def check_model_exists(self):
+        if not os.path.exists(self.saved_model_path):
+            raise FileNotFoundError("Model file not found! Please pass file path using '-m' option")
+        return True
+
+    def _formate_output(self, output_pos_tags, original_sentence):
+        formated_ls = []
+        sentence_ls = original_sentence.split(" ")
+        for i in range(len(sentence_ls)):
+            formated_ls.append("{0}/{1}".format(sentence_ls[i],output_pos_tags[i]))
+        
+        return ' '.join(formated_ls)
+
+    def _hmm_viterbi_algo(self, words:list, observations_len:int, states:list, states_len:int):
+        viterbi = np.array([[0.0]*observations_len]*states_len) #states_len+2,observations_len
+        backpointer = np.array([[-1]*observations_len]*states_len)
+        
+        for s in range(1,states_len-1):
+            ans = self.transition_prob_df.loc['START'][states[s]]*self.emission_prob_df.loc[states[s]][words[0]]
+            viterbi[s][0] = ans
+            backpointer[s][0] = 0
+        
+        for step in range(1, observations_len):
+            
+            for state_no in range(1, states_len-1):
+                max_prob = -1
+                prev_state = -1
+                for pstate_no in range(1, states_len-1):
+                    current_prob = viterbi[pstate_no][step-1]*self.transition_prob_df.loc[states[pstate_no]][states[state_no]]*self.emission_prob_df.loc[states[state_no]][words[step]]
+                    if current_prob>max_prob:
+                        max_prob = current_prob
+                        prev_state = pstate_no
+                    
+                viterbi[state_no][step] = max_prob
+                backpointer[state_no][step] = prev_state 
+
+        #Backtrace backpointer array
+        last_word_state_index = np.argmax(viterbi, axis=0)[observations_len-1]
+        res_pos_tag_ls = [self.pos_tags[last_word_state_index]]
+        current_state_index = last_word_state_index
+        current_observations_index = observations_len -1
+
+        while backpointer[current_state_index][current_observations_index]!=0:
+            res_pos_tag_ls.insert(0, self.pos_tags[backpointer[current_state_index][current_observations_index]])
+            current_state_index = backpointer[current_state_index][current_observations_index]
+            current_observations_index-=1
+
+        return res_pos_tag_ls
+
+    def apply_hmm_viterbi(self):
+        self.logger.info("Running HMM on Test Sentences")
+        self._update_pos_tags()
+        with open(self.output_file, "w") as op_f: 
+            for t_sentence in self.test_in_sentences :
+                try:
+                    words = t_sentence.split(" ")
+
+                    res_pos_tags = self._hmm_viterbi_algo(words, len(words), self.pos_tags, len(self.pos_tags))
+                    res_str = self._formate_output(res_pos_tags, t_sentence)
+                    self.results.append(res_str)
+                    op_f.write(res_str)
+                    op_f.write("\n")
+                except  KeyError as kex:
+                    self.logger.info("Word not found in vocab. Ignoring sentence '{}' ".format(t_sentence))
+                except Exception as ex:
+                    self.logger.error(ex)
+    
+    def read_model(self):
+        try:
+            hdf = pd.HDFStore(self.saved_model_path, mode='r')
+
+            self.emission_prob_df = hdf.get('/emission_prob_df')
+            self.transition_prob_df = hdf.get('/transition_prob_df')
+            hdf.close()
+        except Exception as ex:
+            self.logger.error(ex)
+            raise Exception("Error while reading model file.")
+    
+    def _update_pos_tags(self):
+        self.pos_tags = self.transition_prob_df.index.tolist()
+        
+        self.pos_tags.remove('START')
+        self.pos_tags.remove('END')
+
+        self.pos_tags.insert(0, 'START')
+        self.pos_tags.append('END')
+    
+    def print_results(self):
+        print("********Output********")
+        for ele in self.results:
+            print(ele)
+
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument('sentences', help='Sentence/Test file path contaning list of input sentences (Each at newline)')
+    parser.add_argument('-o', '--output', default="./test.txt", help='Test Output file')
+    parser.add_argument('-f', '--from_file', action='store_true',
+            help='Read Input Sentences to train from a file') 
+    parser.add_argument("-m", "--model", default="./train_model/model.h5", help="Model file path ")
+    parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                    action="store_true")
+   
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+    if args.verbose:
+        logging.basicConfig(level=logging.INFO)
+    
+    sentences_ls = []
+    if args.from_file:
+        with open(args.sentences) as f:
+            for sentence in f:
+                sentence = sentence.strip(" \n")
+                sentences_ls.append(sentence)
+    else:
+        sentences_ls.append(args.sentences)
+
+    hmm = HMM_Client(sentences_ls, args.output, args.model)
+    
+    if hmm.check_model_exists():
+        hmm.read_model()
+        hmm.apply_hmm_viterbi()
+    
+    if args.verbose:
+        hmm.print_results()
+
+if __name__ == '__main__':
+    main()
---
+++ hmm_learn.py
@@ -0,0 +1,133 @@
+import nltk
+import os
+from nltk.corpus import brown as corpus
+import numpy as np
+import pandas as pd
+import logging
+from argparse import ArgumentParser
+from tqdm import tqdm
+
+
+class HMMLearn:
+    logger = logging.getLogger(__name__)
+    
+    def __init__(self, training_file, model_dump_path):
+        self.training_file = training_file
+        self.tags_sequence_ls = []
+        self.tag_word_tupl_ls = []
+        self.unique_tags = {"START", "END"} #Without START and END state
+        self.unique_words = set()
+        self.model_dump_path = model_dump_path
+
+    def _download_brown_corpus(self):
+        self.logger.info("Downloading NLTK 'brown' Corpus and 'universal-tagset' for POS Tagging")
+        nltk.download('brown')
+        nltk.download('universal_tagset')
+
+    def _eval_emission_probabilities(self):
+        self.logger.info("Calculating Emission Probabilities...")
+        unique_words_ls=list(self.unique_words)
+        len_unique_words = len(self.unique_words)
+        unique_tags_ls = list(self.unique_tags)
+        len_unique_tags = len(self.unique_tags)
+        
+        cfd_tag_words= nltk.ConditionalFreqDist(self.tag_word_tupl_ls)
+        cpd_tag_words = nltk.ConditionalProbDist(cfd_tag_words, nltk.MLEProbDist)
+        emission_prob_df = pd.DataFrame(index=unique_tags_ls, columns=unique_words_ls, data=np.zeros((len_unique_tags, len_unique_words)))
+        
+        rowx,coly=emission_prob_df.shape
+        for i in tqdm(range(int(rowx))):
+            for j in range(int(coly)):
+                emission_prob_df.at[unique_tags_ls[i],unique_words_ls[j] ]= cpd_tag_words[unique_tags_ls[i]].prob(unique_words_ls[j]) 
+        
+        return emission_prob_df
+
+    def _eval_transition_probabilities(self):
+        self.logger.info("Calculating Transition Probabilities...")
+        cfd_tags = nltk.ConditionalFreqDist(nltk.bigrams(self.tags_sequence_ls))
+        unique_tags_ls = list(self.unique_tags)
+        len_unique_tags = len(self.unique_tags)
+        
+        transition_prob_df = pd.DataFrame(index=unique_tags_ls, columns=unique_tags_ls, 
+                            data=np.zeros((len_unique_tags, len_unique_tags)))
+        
+        rowi,colj=transition_prob_df.shape
+        for i in tqdm(range(rowi)):
+            for j in range(colj):
+                transition_prob_df.at[unique_tags_ls[i],unique_tags_ls[j] ]=(cfd_tags[unique_tags_ls[i]][unique_tags_ls[j]])/self.tags_sequence_ls.count(unique_tags_ls[i])
+        
+        return transition_prob_df
+    
+    def _is_training_file_present(self):
+        return True if self.training_file else False
+
+    def _convert_tuples(self):
+        sentences = []
+        with open(self.training_file, "r") as tfile:
+            sentence_ls = []
+            for line in tfile:
+                line = line.strip(" \n")
+                for word_tag in line.split(" "):
+                    word,tag = word_tag.split("/")
+                    sentence_ls.append((word, tag))
+                sentences.append(sentence_ls)
+        return sentences
+
+    def load_data(self):
+        self.logger.info("Loading Train Data....")
+        if self._is_training_file_present():
+            tagged_sentences = self._convert_tuples()      
+        else: 
+            self._download_brown_corpus()
+            tagged_sentences = corpus.tagged_sents(tagset='universal')
+    
+        for sentence in tagged_sentences: # get tagged sentences
+            self.tag_word_tupl_ls.append( ("START", "START") )
+            self.tags_sequence_ls.append("START")
+            for (word, tag) in sentence:
+                self.tags_sequence_ls.append(tag)
+                self.tag_word_tupl_ls.append( (tag, word) ) 
+                self.unique_words.add(word)
+                self.unique_tags.add(tag)
+            self.tag_word_tupl_ls.append(("END", "END"))
+            self.tags_sequence_ls.append("END")
+
+    def learn_training_probabilities(self):
+        try:
+            transition_prob_df = self._eval_transition_probabilities()
+            emission_prob_df = self._eval_emission_probabilities()
+
+            save_path = os.path.join(self.model_dump_path, "model.h5")
+            hdf = pd.HDFStore(save_path)
+            hdf.put("transition_prob_df", transition_prob_df, data_columns=True)
+            hdf.put("emission_prob_df", emission_prob_df, data_columns=True)
+            self.logger.info("Saving model to {}".format(save_path))
+            hdf.close()
+        except Exception as ex:
+            raise Exception("Error while Learning Probabilities!")
+
+
+def get_args():
+    parser = ArgumentParser(description='Train model for Parts of Speech Tagging.')
+    parser.add_argument('-it', '--input_file', help='Input Train file. If not provided nltk brown corpus used for training.')
+    parser.add_argument('-od', '--output_dir', default="./train_model/", help='Path of models output dir') 
+    parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                    action="store_true")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    
+    args = get_args()
+    if args.verbose:
+        logging.basicConfig(level=logging.INFO)
+
+    training_filepath = args.input_file
+    model_dump_dirpath = args.output_dir
+    print(model_dump_dirpath)
+    hmm_learn = HMMLearn(training_filepath, model_dump_dirpath)
+    hmm_learn.load_data()
+    hmm_learn.learn_training_probabilities()   
+
+
+
